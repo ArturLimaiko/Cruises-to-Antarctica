a32c7be26c3bae3c7186b828e9c993d0f41ed47e
Artur <alimayko91@gmail.com>
2023-04-25 12:44:39 +0300

    test push

---
+++ source/js/main.js
@@ -0,0 +1,122 @@
+import {iosVhFix} from './utils/ios-vh-fix';
+import {initModals} from './modules/modals/init-modals';
+import {Form} from './modules/form-validate/form';
+import {headerBurger} from './modules/header-burger';
+import {scroll} from './modules/scroll';
+const inputsPhone = document.querySelectorAll('[data-phone-number]');
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  headerBurger();
+  scroll();
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initModals();
+    const form = new Form();
+    window.form = form;
+    form.init();
+  });
+});
+
+const prefixNumber = (str) => {
+  if (str === '7') {
+    return '7 (';
+  }
+  if (str === '8') {
+    return '8 (';
+  }
+  if (str === '9') {
+    return '7 (9';
+  }
+  return '7 (';
+};
+
+inputsPhone.forEach((el) => el.addEventListener('input', () => {
+  const value = el.value.replace(/\D+/g, '');
+  const numberLength = 11;
+
+  let result;
+  if (el.value.includes('+8') || el.value[0] === '8') {
+    result = '';
+  } else {
+    result = '+';
+  }
+
+  for (let i = 0; i < value.length && i < numberLength; i++) {
+    switch (i) {
+      case 0:
+        result += prefixNumber(value[i]);
+        continue;
+      case 4:
+        result += ') ';
+        break;
+      case 7:
+        result += '-';
+        break;
+      case 9:
+        result += '-';
+        break;
+      default:
+        break;
+    }
+    result += value[i];
+  }
+  el.value = result;
+}));
+
+const forms = document.querySelectorAll('.form');
+
+if (document.querySelector('.form')) {
+  forms.forEach((form) => {
+    form.addEventListener('submit', (evt) => {
+      if (!(form.querySelector('[data-phone-number]').value.length < 18)) {
+        return;
+      } else {
+        evt.preventDefault();
+        showError(form.querySelector('[data-phone-number]'));
+      }
+    });
+  });
+}
+
+const showError = (elem) => {
+  elem.classList.add('error');
+  setTimeout(() => {
+    elem.classList.remove('error');
+  }, 1000);
+};
+
+// ---------------------------------
+
+// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+
+// привязывайте js не на классы, а на дата атрибуты (data-validate)
+
+// вместо модификаторов .block--active используем утилитарные классы
+// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
+// .select.select--opened ❌ ---> [data-select].is-open ✅
+
+// выносим все в дата атрибуты
+// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+
+// для адаптивного JS используется matchMedia и addListener
+// const breakpoint = window.matchMedia(`(min-width:1024px)`);
+// const breakpointChecker = () => {
+//   if (breakpoint.matches) {
+//   } else {
+//   }
+// };
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+// используйте .closest(el)
---
+++ source/js/modules/header-burger.js
@@ -0,0 +1,35 @@
+export const headerBurger = () => {
+  const header = document.querySelector('.header');
+
+  if (!header) {
+    return;
+  }
+
+  const headerButton = header.querySelector('.header__button');
+  const headerLinks = header.querySelectorAll('.header__link');
+
+  header.classList.remove('header--nojs');
+
+  const onToggleMenu = () => {
+    header.classList.toggle('header--closed');
+    header.classList.toggle('header--opened');
+    document.body.classList.toggle('scroll-lock');
+  };
+
+  headerButton.addEventListener('click', () => onToggleMenu());
+
+  document.addEventListener('click', (e) => {
+
+    const click = e.composedPath().includes(header);
+
+    if (!click) {
+      header.classList.remove('header--opened');
+      header.classList.add('header--closed');
+      document.body.classList.remove('scroll-lock');
+    }
+  });
+
+  headerLinks.forEach((link) => {
+    link.addEventListener('click', () => onToggleMenu());
+  });
+};
